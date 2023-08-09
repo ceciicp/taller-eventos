2760049eec029236577262695c9e1e36a14ef022
ceciicp <ccanovaspisco@gmail.com>
2023-08-09 14:56:07 -0300

    stopPropagation

--- eventos.js
+++ eventos.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded',() =>
     { let midiv= document.getElementById("divv");
 
-    midiv.addEventListener('click',()=>{alert("Hola! Soy el div");})
-    
+    let botoncito= document.getElementById("btn");
+        botoncito.addEventListener('click',(event)=>{alert("Hola!");
+        event.stopPropagation();})
+
+    midiv.addEventListener('click',()=>{
+    alert("Hola! Soy el div");})
+
 })
+

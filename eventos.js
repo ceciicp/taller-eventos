e8fc8cc089e84b73057a508b2da9cd6de412c60b
ceciicp <ccanovaspisco@gmail.com>
2023-08-09 14:05:52 -0300

    Alerta en el div

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded',() =>
+    { let midiv= document.getElementById("divv");
+
+    midiv.addEventListener('click',()=>{alert("Hola! Soy el div");})
+    
+})
